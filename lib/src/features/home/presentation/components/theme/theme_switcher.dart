8f3d6ed81763c0a8be6e517ea4c6c226dcf7656b
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-04 21:09:08 +0100

    feat: implement theme switcher

---
+++ lib/src/features/home/presentation/components/theme/theme_switcher.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kanban_task_manager/src/features/theme/logic/bloc/theme_state.dart';
+import 'package:kanban_task_manager/src/shared/shared.dart';
+
+class ThemeSwitcher extends StatelessWidget {
+  const ThemeSwitcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (_, state) {
+        return Container(
+          width: 251,
+          height: 48,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: theme.surface,
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgAsset(iconLightTheme),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: 40,
+                  height: 20,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Switch(
+                      value: state.isDarkMode,
+                      activeColor: theme.primary,
+                      inactiveTrackColor: theme.primary,
+                      inactiveThumbColor: appColors.white,
+                      thumbColor: WidgetStatePropertyAll(appColors.white),
+                      splashRadius: 0.0,
+                      trackOutlineColor:
+                          WidgetStatePropertyAll(Colors.transparent),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      onChanged: (_) {
+                        context.read<ThemeBloc>().add(ToggleThemeEvent());
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              SvgAsset(iconDarkTheme),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:kanban_task_manager/src/shared/shared.dart';
+import 'package:kanban_task_manager/src/features/home/presentation/components/components.dart';
 
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context).colorScheme;
-    final controller = useTextEditingController();
+    // final theme = Theme.of(context).colorScheme;
 
     return Scaffold(
       body: Center(
@@ -17,49 +16,7 @@ class HomeScreen extends HookWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AppButton(
-                title: 'Small Button',
-                onTap: () {},
-              ),
-              YBox(20),
-              AppButton(
-                title: 'Red Button',
-                color: theme.error,
-                hoverColor: theme.errorContainer,
-                onTap: () {},
-              ),
-              YBox(20),
-              AppButton(
-                title: 'Secondary Button',
-                color: theme.secondary,
-                hoverColor: theme.secondaryContainer,
-                textColor: theme.primary,
-                onTap: () {},
-              ),
-              YBox(20),
-              AppButton.large(
-                title: 'Large Button',
-                onTap: () {},
-              ),
-              YBox(20),
-              AppCheckboxTile(
-                label: 'Eat well',
-                isChecked: false,
-                onToggle: () {},
-              ),
-              YBox(20),
-              AppTextField(
-                controller: controller,
-                labelText: 'TextField',
-                hintText: 'Example textfield',
-              ),
-              YBox(20),
-              AppDropdown(
-                labelText: 'Dropdown',
-                initialSelectedValue: 'First',
-                items: ['First', 'Second', 'Third'],
-                onChanged: (value) {},
-              ),
+              ThemeSwitcher(),
             ],
           ),
         ),
