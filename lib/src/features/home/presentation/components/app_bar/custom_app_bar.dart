7136243efb5e87fcf5ca4d184dd42202119b6831
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-05 17:45:05 +0100

    feat: update custom appbar

--- lib/src/features/home/presentation/components/app_bar/custom_app_bar.dart
+++ lib/src/features/home/presentation/components/app_bar/custom_app_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kanban_task_manager/src/features/home/logic/cubits/cubits.dart';
+import 'package:kanban_task_manager/src/features/theme/logic/bloc/theme_state.dart';
 import 'package:kanban_task_manager/src/shared/shared.dart';
 
 class CustomAppBar extends StatelessWidget {
@@ -9,57 +12,93 @@ class CustomAppBar extends StatelessWidget {
     final theme = Theme.of(context).colorScheme;
     final isMobile = DeviceType(context).isMobile;
 
-    return Container(
-      height: 76,
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-        color: theme.tertiary,
-        border: Border(
-          bottom: BorderSide(
-            color: theme.outline,
-          ),
-        ),
-      ),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: isMobile
-                ? () {
-                    //TODO: Show dialog
-                  }
-                : null,
-            child: MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  AppText(
-                    'Platform stuff',
-                    fontSize: 24,
-                    overflow: TextOverflow.ellipsis,
+    return BlocBuilder<SideBarCubit, bool>(
+      builder: (_, showSidebar) {
+        return BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (_, themeState) {
+            return Container(
+              height: 76,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: theme.tertiary,
+                border: Border(
+                  bottom: BorderSide(
+                    color: theme.outline,
                   ),
-                  if (isMobile) ...[
-                    XBox(10),
-                    SvgAsset(arrowDown),
-                  ]
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: isMobile
+                        ? () {
+                            //TODO: Show dialog
+                          }
+                        : null,
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (!showSidebar || isMobile)
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 15),
+                                  child: SvgAsset(
+                                    isMobile
+                                        ? logoMobile
+                                        : (themeState.isDarkMode
+                                            ? logoLight
+                                            : logoDark),
+                                  ),
+                                ),
+                                Container(
+                                  color: theme.outline,
+                                  width: 1,
+                                  height: double.infinity,
+                                  margin: EdgeInsets.only(right: 15),
+                                ),
+                              ],
+                            ),
+                          AppText(
+                            'Platform Launch',
+                            fontSize: 20,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          if (isMobile) ...[
+                            XBox(10),
+                            SvgAsset(arrowDown),
+                          ]
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Spacer(),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AddTaskButton(
+                        inactive: false,
+                        onTap: () {},
+                      ),
+                      XBox(15),
+                      PopupMenuButton(
+                        child: SvgAsset(iconverticalEllipsis),
+                        itemBuilder: (context) {
+                          return [];
+                        },
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-          ),
-          Spacer(),
-          AddTaskButton(
-            inactive: false,
-            onTap: () {},
-          ),
-          XBox(15),
-          PopupMenuButton(
-            child: SvgAsset(iconverticalEllipsis),
-            itemBuilder: (context) {
-              return [];
-            },
-          ),
-        ],
-      ),
+            );
+          },
+        );
+      },
     );
   }
 }
